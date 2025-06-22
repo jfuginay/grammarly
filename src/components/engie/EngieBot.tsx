85586ee7c37f42cf27027c771f85c8b57ea6ff66
J Wylie <j@engindearing.soy>
2025-06-21 18:55:08 -0700

    Fix Engie visibility issue - remove undefined isVisible check
    - Remove visibility check that was preventing Engie from appearing
    - Engie should always be visible as per dashboard comments
    - The isVisible property doesn't exist in EngieState interface
    - This fixes the issue where Engie disappeared after merge
    
    Fixes: Engie not appearing on dashboard

--- src/components/engie/EngieBot.tsx
+++ src/components/engie/EngieBot.tsx
@@ -153,8 +153,7 @@ export const EngieBot: React.FC<EngieProps> = (props) => {
   const handleToggleGrokMode = () => controller.toggleGrokMode();
   const handleResearchWithGrok = (topic: string) => controller.researchWithGrok(topic);
 
-  if (!state.isVisible) return null;
-
+  // Engie is always visible - removed visibility check
   return (
     <>
       {/* Engie Container with Relative Positioning */}
